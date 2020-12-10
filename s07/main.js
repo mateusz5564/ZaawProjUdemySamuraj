df860f53bf49d01864734f49c44e42616cd631ae
mateusz5564 <mateusz5564@gmail.com>
2020-12-10 22:02:11 +0100

    s07 project - code refactor

--- s07/main.js
+++ s07/main.js
@@ -1,22 +1,17 @@
-const form = document.querySelector('.form');
-const results = document.querySelector('.results');
-let users;
+const form = document.querySelector(".form");
+const results = document.querySelector(".results");
 
 function getData(e) {
   e.preventDefault();
-  const genderValue = e.target.elements['gender'].value;
-  const numberValue = e.target.elements['number'].value;
+  const genderValue = e.target.elements["gender"].value;
+  const numberValue = e.target.elements["number"].value;
   const url = `https://randomuser.me/api/?results=${numberValue}&gender=${genderValue}`;
+  results.innerHTML = '';
 
   fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .catch(err => console.log(err))
-  .then(json => {
-    users = json.results;
-    renderResults(users);
-  })
+    .then((response) => response.json())
+    .catch(err => console.log(err))
+    .then(json => renderResults(json.results))
 }
 
 function renderResults(users) {
@@ -26,22 +21,22 @@ function renderResults(users) {
     const userElement = createUserElement(username, avatar);
 
     results.appendChild(userElement);
-  })
+  });
 }
 
 function createUserElement(username, avatar) {
-  const div = document.createElement('div');
-  const img = document.createElement('img');
-  const span = document.createElement('span');
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  const span = document.createElement("span");
 
-  div.classList.add('result');
-  img.classList.add('result__img');
-  img.setAttribute('src', avatar);
+  div.classList.add("result");
+  img.classList.add("result__img");
+  img.setAttribute("src", avatar);
   span.textContent = username;
   div.appendChild(img);
   div.appendChild(span);
-  
+
   return div;
 }
 
-form.addEventListener('submit', getData)
+form.addEventListener("submit", getData);
