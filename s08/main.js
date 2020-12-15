b87490ae2aba984a28303473fec637f24c78d04d
mateusz5564 <mateusz5564@gmail.com>
2020-12-15 18:18:43 +0100

    scrolling by keyboard

--- s08/main.js
+++ s08/main.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const scroller = new Scroller('#scroller');
+  const scroller = new Scroller("#scroller");
   document.addEventListener("wheel", scroller.listenScroll);
 
-  document.addEventListener('swipeUp', (e) => {
-    scroller.scroll(1);
-  });
-  document.addEventListener('swipeDown', (e) => {
-    scroller.scroll(-1);
-  });
+  //mobile
+  document.addEventListener("swipeUp", (e) => scroller.scroll(1));
+  document.addEventListener("swipeDown", (e) => scroller.scroll(-1));
 
+  //keyboard 40/38
+  document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+      case 40:
+        return scroller.scroll(1);
+      case 38:
+        return scroller.scroll(-1);
+      default:
+        return;
+    }
+  });
 });
