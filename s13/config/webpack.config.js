7c7d95949af35823646443ce990761c30d0f207d
mateusz5564 <mateusz5564@gmail.com>
2020-12-31 20:21:59 +0100

    s14e03 mini css extract plugin

--- s13/config/webpack.config.js
+++ s13/config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   stats: {
@@ -9,19 +10,25 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "[name]-[contenthash:4].js",
+    filename: "js/[name]-[contenthash:4].js",
     path: path.resolve(__dirname, "../", "build"),
   },
   module: {
     rules: [
       { test: /\.txt$/, use: "raw-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       template: "src/templates/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name]-[contenthash:4].css",
     }),
   ],
   devServer: {
