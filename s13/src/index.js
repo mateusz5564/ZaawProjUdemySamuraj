c668e9d3142a1b3c9943d1513599716f36167398
mateusz5564 <mateusz5564@gmail.com>
2021-01-02 20:52:37 +0100

    s14e10 add class Creator to test Babel

--- s13/src/index.js
+++ s13/src/index.js
@@ -3,8 +3,15 @@ import helloworld from "./data/helloworld.txt";
 import "./sass/index.scss";
 import "./components/footer";
 import addImage from './utils/image';
+import Creator from './utils/creator';
 
 message("works after boundling");
 messageDOM(helloworld);
 
 addImage('div')
+
+const el1 = new Creator();
+el1.addBgc('red');
+const el2 = new Creator();
+el2.addBgc('blue');
+const el3 = new Creator();
---
+++ s13/src/utils/creator.js
@@ -0,0 +1,11 @@
+export default class {
+  constructor() {
+    this.element = document.createElement("div");
+    this.element.style.height = "100px";
+    document.body.appendChild(this.element);
+  }
+
+  addBgc(color) {
+    this.element.style.backgroundColor = color;
+  }
+}
