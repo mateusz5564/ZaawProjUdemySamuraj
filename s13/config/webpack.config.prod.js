e31dc796a6153ada7be517d7acd244b0aa1c51da
mateusz5564 <mateusz5564@gmail.com>
2021-01-04 18:32:17 +0100

    s14e13 move babel configuration from .babelrc to webpack config

--- s13/config/webpack.config.prod.js
+++ s13/config/webpack.config.prod.js
@@ -58,6 +58,10 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
       },
     ],
   },
