f042f79671b97d127462ae960773925157c03751
mateusz5564 <mateusz5564@gmail.com>
2021-01-11 20:01:24 +0100

    s17e06-e07 render and style list, render list or table

--- s17/js/main.js
+++ s17/js/main.js
@@ -9,27 +9,63 @@ const valuesContainer = document.querySelector(".app--values");
 
 const desktopViewport = window.matchMedia("screen and (min-width: 500px)");
 
+const drawValues = (isDesktop) => {
+  isDesktop === true ? drawDesktopValues() : drawMobileValues();
+};
+
 const drawDesktopValues = () => {
   valuesContainer.innerHTML = "";
 
-  let table = document.createElement('table');
-  
-  let thead = document.createElement('thead');
-  thead.innerHTML = "<tr><th>Nazwa</th><th>Kalorie</th><th>Tłuszcz</th><th>Węglowodany</th></tr>"
-  
-  let tbody = document.createElement('tbody');
+  let table = document.createElement("table");
+
+  let thead = document.createElement("thead");
+  thead.innerHTML =
+    "<tr><th>Nazwa</th><th>Kalorie</th><th>Tłuszcz</th><th>Węglowodany</th></tr>";
+
+  let tbody = document.createElement("tbody");
 
   values.forEach((item) => {
-    let row = document.createElement('tr');
+    let row = document.createElement("tr");
     row.innerHTML = `<td>${item.name}</td><td>${item.calories}</td><td>${item.fat}</td><td>${item.carbs}</td>`;
 
     tbody.appendChild(row);
-  })
-  
+  });
+
   table.appendChild(thead);
   table.appendChild(tbody);
   valuesContainer.appendChild(table);
-}
+};
 
+const drawMobileValues = () => {
+  valuesContainer.innerHTML = "";
 
-drawDesktopValues();
+  let list = document.createElement("ul");
+
+  values.forEach((item) => {
+    let element = document.createElement("li");
+
+    let name = document.createElement("div");
+    name.innerHTML = `<strong>Nazwa: </strong>${item.name}`;
+    let calories = document.createElement("div");
+    calories.innerHTML = `<strong>Kalorie: </strong>${item.calories}`;
+    let fat = document.createElement("div");
+    fat.innerHTML = `<strong>Tłuszcz: </strong>${item.fat}`;
+    let carbs = document.createElement("div");
+    carbs.innerHTML = `<strong>Węglowodany: </strong>${item.carbs}`;
+
+    element.appendChild(name);
+    element.appendChild(calories);
+    element.appendChild(fat);
+    element.appendChild(carbs);
+
+    list.appendChild(element);
+  });
+
+  valuesContainer.appendChild(list);
+};
+
+drawValues(desktopViewport.matches);
+desktopViewport.addListener(isDesktop => {
+  drawValues(isDesktop.matches);
+});
+
