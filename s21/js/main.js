238fef1efafde03516abe650d795a21fd8b91722
mateusz5564 <mateusz5564@gmail.com>
2021-01-13 19:43:49 +0100

    s21e02 draw star

--- s21/js/main.js
+++ s21/js/main.js
@@ -14,8 +14,38 @@ class Sky {
         this.ctx.fillRect(0, 0, this.width, this.height);
     }
 
+    drawStar(star) {
+        this.ctx.save();
+
+        this.ctx.fillStyle = star.color;
+
+        this.ctx.beginPath();
+
+        this.ctx.translate(star.x, star.y);
+        this.ctx.moveTo(0, 0 - star.radius);
+
+        for(let i = 0; i < 5; i++) {
+            this.ctx.rotate((Math.PI / 180) * 36);
+            this.ctx.lineTo(0, 0 - star.radius * 0.6);
+            this.ctx.rotate((Math.PI / 180) * 36);
+            this.ctx.lineTo(0, 0 - star.radius);
+        }
+
+        this.ctx.fill();
+        this.ctx.restore();
+        
+    }
+
+    draw() {
+        console.log("draw")
+        window.requestAnimationFrame(() => this.draw());
+    }
+
     run() {
         this.initCanvas();
+        this.drawStar({
+            x: 100, y: 100, color: "#fff", radius: 100,
+        });
     }
 }
 
