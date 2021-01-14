6206e6cf0164d98e4ef7a8aa7427fb56b3e256a0
mateusz5564 <mateusz5564@gmail.com>
2021-01-14 20:37:08 +0100

    s22e04 spinner in js using requestAnimationFrame

--- s22/spinner.js
+++ s22/spinner.js
@@ -1,3 +1,5 @@
+// SPINNER v2
+
 const spinner = document.querySelector('.v2')
 
 const fps = 1000 / 60;
@@ -5,10 +7,28 @@ let deg = 0;
 const degChange = 6;
 
 const rotate = () => {
-  deg += 6;
+  deg += degChange;
   spinner.style.transform = `rotate(${deg}deg)`;
 
   setTimeout(rotate, fps);
 }
 
 rotate();
+
+
+
+// SPINNER v3
+
+const spinnerRAF = document.querySelector('.v3');
+
+let degRAF = 0;
+const degChangeRAF = 6;
+
+const rotateRAF = () => {
+  degRAF += degChangeRAF;
+  spinnerRAF.style.transform = `rotate(${degRAF}deg)`;
+
+  requestAnimationFrame(rotateRAF);
+}
+
+rotateRAF();
